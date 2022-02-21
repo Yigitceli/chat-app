10170780838312ea9eb733693a45534c3326b812
Yigitceli <Yigit.celi@outlook.com>
2022-02-21 22:34:26 +0300

    Register setup

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,16 +2,30 @@ import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { signWithGoogle, signInWithTwitter } from "../../firebaseconfig";
 import { BsTwitter } from "react-icons/bs";
-import { UserCredential } from "firebase/auth";
+import { sendPasswordResetEmail, UserCredential } from "firebase/auth";
 import { setAccessToken, setRefreshToken } from "../services/authService";
 import { Link } from "react-router-dom";
-import FormInput from "../components/formInput";
+import { get, SubmitHandler, useForm } from "react-hook-form";
+
+type FormValues = {
+  email: string;
+  name: string;
+  surname: string;
+  password: string;
+  verifyPassword: string;
+  checkBox: boolean;
+};
 
 export default function Register() {
-  const [values, setValues] = useState<object | null>(null);
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<FormValues>();
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
+  const FormSubmitHandler: SubmitHandler<FormValues> = (data: FormValues) => {
+    console.log(data);
   };
 
   return (
@@ -26,67 +40,115 @@ export default function Register() {
         </h1>
       </div>
       <div className="flex flex-col h-full w-full lg:w-1/2 justify-center items-center">
-        <form className="flex flex-col gap-10 w-200 md:w-200">
+        <form
+          className="flex flex-col gap-10 w-200 md:w-200"
+          onSubmit={handleSubmit(FormSubmitHandler)}
+        >
           <div className="flex flex-col gap-3">
             <h2 className="text-white font-bold text-4xl">Register</h2>
             <p className="text-white">Fill the form for registiration.</p>
           </div>
           <div className="flex flex-col gap-4">
-            <FormInput
-              placeHolder="name@hotmail.com"
-              name="email"
-              type="email"
-              id="email"
-              label="Your Email"
-              error="Email is not valid."
-              onChange={onChange}
-            />
-            <FormInput
-              placeHolder="Your Name"
-              name="name"
-              type="text"
-              id="name"
-              onChange={onChange}
-              label="Name"
-              error="Name can`t be empty."
-            />
-            <FormInput
-              placeHolder="Your Surname"
-              name="surname"
-              type="text"
-              id="surname"
-              onChange={onChange}
-              label="Surname"
-              error="Surname can`t be empty."
-            />
-            <FormInput
-              placeHolder="At least 8 character, number and symbols"
-              name="password"
-              type="password"
-              id="password"
-              onChange={onChange}
-              label="Your Password"
-              error="Password should be 8-20 characters and include at least 1 letter,
-              1 number and 1 special character!"
-            />
-            <FormInput
-              placeHolder="At least 8 character, number and symbols"
-              name="verifyPassword"
-              type="password"
-              id="verify-password"
-              label="Verify Your Password"
-              error="Passwords do not match!"
-              onChange={onChange}
-            />
+            <div className="flex flex-col gap-4">
+              <label className="text-white test">Your Email</label>
+              <input
+                className="focus:outline-none rounded-md py-2 px-2 invalid:border-red-500"
+                placeholder="name@hotmail.com"
+                type="text"
+                id="email"
+                {...register("email", {
+                  required: true,
+                  pattern: RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"),
+                })}
+              />
+              {errors.email && (
+                <span className="text-secondary">Email is not valid.</span>
+              )}
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="text-white test">Name</label>
+              <input
+                className="focus:outline-none rounded-md py-2 px-2 invalid:border-red-500"
+                placeholder="Your Name"
+                type="text"
+                id="name"
+                {...register("name", { required: true, minLength: 2 })}
+              />
+              {errors.name && (
+                <span className="text-secondary">Name can`t be empty.</span>
+              )}
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="text-white test">Surname</label>
+              <input
+                className="focus:outline-none rounded-md py-2 px-2 invalid:border-red-500"
+                placeholder="Your Surname"
+                type="text"
+                id="surname"
+                {...register("surname", { required: true, minLength: 2 })}
+              />
+              {errors.surname && (
+                <span className="text-secondary">Surname can`t be empty.</span>
+              )}
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="text-white test">Your Password</label>
+              <input
+                className="focus:outline-none rounded-md py-2 px-2 invalid:border-red-500"
+                placeholder="Min 8 - Max 20 character"
+                type="text"
+                id="password"
+                {...register("password", {
+                  required: true,
+                  pattern: RegExp(
+                    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*.-_])(?=.{8,})"
+                  ),
+                })}
+              />
+              {errors.password && (
+                <span className="text-secondary">
+                  Password should be 8-20 characters and include at least 1
+                  lowercase, 1 uppercase, 1 number and 1 special character!
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="text-white test">Verify Your Password</label>
+              <input
+                className="focus:outline-none rounded-md py-2 px-2 invalid:border-red-500"
+                placeholder="At least 8 character, number and symbols"
+                type="password"
+                id="verifyPassword"
+                {...register("verifyPassword", {
+                  required: true,
+                  validate: (value) => value === getValues("password"),
+                })}
+              />
+              {errors.verifyPassword && (
+                <span className="text-secondary">Passwords do not match!</span>
+              )}
+            </div>
           </div>
           <label
             htmlFor="remember"
-            className="flex items-center gap-4 text-white"
+            className="flex gap-1 items-center text-white flex-col"
           >
-            <input type="checkbox" name="remember" id="remember" />
-            Accept the Terms and Conditions of the site and the information data
-            policy.
+            <div className="flex gap-4 items-center">
+              <input
+                type="checkbox"
+                id="checkbox"
+                {...register("checkBox", { required: true })}
+              />
+              <span>
+                Accept the Terms and Conditions of the site and the information
+                data policy.
+              </span>
+            </div>
+            {errors.checkBox && (
+              <span className="text-secondary">You have to accept terms.</span>
+            )}
           </label>
+
           <button
             type="submit"
             className="bg-secondary w-full rounded-md p-1 py-2 text-white font-bold"
